7e0b4091e88fd3347d51387989eae38e68729cd6
Joyce Chen <chenjy@mit.edu>
2017-04-12 03:40:11 -0400

    Added more responses

--- ronbot.js
+++ ronbot.js
@@ -12,6 +12,8 @@ var rtm = new RtmClient(BOT_TOKEN);
 
 const MESSAGE = 'message';
 let GENERAL; // general channel id
+let RANDOM; // random channel id
+let TESTING;
 
 var users = {}; // maps user ID to info
 var lastMessages = []; // stores message text in sequence
@@ -24,13 +26,19 @@ web.users.list(function(err, info) {
   });
 });
 
+//console.log(getChannelEntity('#bot-testing'));
+
 web.channels.list((err, info) => {
   info.channels.forEach((channel) => {
     if(channel.name === 'general') {
       GENERAL = channel.id;
+    } 
+    if (channel.name === 'random') {
+      RANDOM = channel.id;
     }
   });
 });
+
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
@@ -39,8 +47,13 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
 rtm.start();
 
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function() {
-  //rtm.sendMessage("Testing, 123", GENERAL);
+  //rtm.sendMessage("Boo-yah! I'm back from the dead!", RANDOM);
 });
+
+var responses = {};
+responses[/(hello|hi) ronbot/i] = ""
+
+
 
 rtm.on(RTM_CLIENT_EVENTS.RAW_MESSAGE, function handleRtmMessage(message) {
   message = JSON.parse(message);
@@ -55,9 +68,54 @@ rtm.on(RTM_CLIENT_EVENTS.RAW_MESSAGE, function handleRtmMessage(message) {
 
   }
   
-  if(text.match(/(hello|hi) ronbot/i)) {
+  if(text.match(/(hello|hey|yo|hi) ronbot/i)) {
     if(!users[message.user]) return;
-    return rtm.sendMessage(`hello ${users[message.user].name}`, message.channel);
+    return rtm.sendMessage(`hey ${users[message.user].name}`, message.channel);
+  } 
+
+  if(text.match(/sup ronbot/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`sup ${users[message.user].name}`, message.channel);
+  }
+
+  if(text.match(/booyah/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`BOO-YAH!`, message.channel);
+  }
+
+  if(text.match(/favorite food/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`The Naco, where Taco meets Nacho`, message.channel);
+  }
+
+  if(text.match(/did it/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`Not too shabby, ${users[message.user].name}`, message.channel);
+  }
+
+  if(text.match(/monkey(s?)( ninja)?/i) || text.match(/gorilla(s?)/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`AHHHHHHHHHHHHHH`, message.channel);
+  }
+
+  if(text.match(/how's shego/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`green, very green`, message.channel);
+  }
+
+  if(text.match(/how's drakken/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`a bit blue`, message.channel);
+  }
+
+  if(text.match(/who rocks/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`RUFUS`, message.channel);
+  }
+
+  if(text.match(/where's K(im)?( P)?/i)) {
+    if(!users[message.user]) return;
+    return rtm.sendMessage(`im on a solo mish :sunglasses:`, message.channel);
   }
 });
 
